671d048773489f18a61d42414c7fe3aaee8700e0
tejalchinoy <110626021+tejalchinoy@users.noreply.github.com>
2024-04-06 12:47:26 -0700

    Update script.js
    Comment out testimonial section

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,9 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 // sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
-
-
+/* 
 // testimonials variables
-//const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -52,7 +51,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
+*/
 
 
 // custom select variables
